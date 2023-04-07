63e5b8f01ceff5dd1d7d418b258b0f19c5c31939
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-07 14:12:06 +0200

    kind rules and small refactor

--- api/controllers/properties.ts
+++ api/controllers/properties.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Propertie from "../models/properties";
 import { Request, Response } from "express";
+import KindRules from "../models/kindRules";
 
 const getPropertieList = (req: Request, res: Response) => {
   Propertie.find()
@@ -10,7 +11,15 @@ const getPropertieList = (req: Request, res: Response) => {
 
 const getPropertie = (req: Request, res: Response) => {
   Propertie.findById(req.params.id)
-    .then((propertie) => res.status(200).json(propertie))
+    .then((propertie) => {
+      if (propertie === null) {
+        res.status(404).json({
+          message: "Propertie not found",
+        });
+      } else {
+        res.status(200).json(propertie);
+      }
+    })
     .catch((err) => res.status(500).json(err));
 };
 
@@ -30,6 +39,35 @@ const propertieCreate = (req: Request, res: Response) => {
     });
 };
 
+const getPropertieRules = async (req: Request, res: Response) => {
+  const propertieId = req.params.id;
+  //check propertie id was provided
+  if (!propertieId) {
+    res.status(404).json({
+      message: "Not found, propertie id is required",
+    });
+    return;
+  }
+
+  Propertie.findById(propertieId)
+    .then((propertie) => {
+      KindRules.find({ kind: propertie?.kind })
+        .then((kindRules) => {
+          res.status(200).json(kindRules);
+        })
+        .catch((reason) =>
+          res.status(500).json({
+            message: "Propertie have not kind rules associated",
+          })
+        );
+    })
+    .catch((reason) => {
+      res.status(404).json({
+        message: "Not found, propertie does not exist",
+      });
+    });
+};
+
 const propertieBuy = async (req: Request, res: Response) => {
   interface IBody {
     ownerId: string;
@@ -41,6 +79,14 @@ const propertieBuy = async (req: Request, res: Response) => {
   if (!propertieId) {
     res.status(404).json({
       message: "Not found, propertie id is required",
+    });
+    return;
+  }
+
+  //check owner id was provided
+  if (!body.ownerId) {
+    res.status(400).json({
+      message: "owner id is required",
     });
     return;
   }
@@ -57,7 +103,7 @@ const propertieBuy = async (req: Request, res: Response) => {
         { owner: body.ownerId },
         { new: true }
       );
-      res.status(200).json(updatedPropertie);
+      res.status(201).json(updatedPropertie);
     });
   } catch (error: any) {
     res.status(500).json({ msg: error.message });
--- api/models/properties.ts
+++ api/models/properties.ts
@@ -1,34 +1,30 @@
 import mongoose, { Model, Schema } from "mongoose";
-
-export type Kind = "Transport" | "Education" | "Health" | "Groceries";
+import { Kind } from "./kindRules";
 
 interface Propertie {
-    name: string;
-    _id?: string;
-    address: string;
-    price: number;
-    income: number;
-    owner?: string;
-    kind: Kind;
-}
-
-interface KindRestrictions {
-    MaxTemperature: { value: number; modifier: number };
-    MinTemperature: { value: number; modifier: number };
-    EnergyConsumption: number;
-    Weather: { sunny: number; rainy: number; cloudy: number };
+  name: string;
+  _id?: string;
+  address: string;
+  price: number;
+  income: number;
+  owner?: string;
+  kind: Kind;
+  lat: number;
+  lng: number;
 }
 
 const propertieSchema = new Schema<Propertie>({
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    price: { type: Number, required: true },
-    income: { type: Number, required: true },
-    owner: { type: String, required: false },
-    kind: { type: String, required: true },
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  price: { type: Number, required: true },
+  income: { type: Number, required: true },
+  owner: { type: String, required: false },
+  kind: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
 });
 
 const PropertieModel: Model<Propertie> =
-    mongoose.models.Entry || mongoose.model("Entry", propertieSchema);
+  mongoose.models.Propertie || mongoose.model("Propertie", propertieSchema);
 
 export default PropertieModel;
