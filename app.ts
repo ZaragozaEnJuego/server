3ac1a2bdf7aea12fce6375566908b87da5b99602
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-13 20:12:12 +0200

    swagger

--- app.ts
+++ app.ts
@@ -4,7 +4,6 @@ import propertiesRouter from "./api/routes/properties";
 import usersRouter from "./api/routes/users";
 import authRouter from "./api/routes/auth";
 
-
 //FOR TESTING LOOK
 //https://dev.to/nathan_sheryak/how-to-test-a-typescript-express-api-with-jest-for-dummies-like-me-4epd
 
@@ -12,15 +11,50 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const passport = require('passport');
-const session = require('express-session');
+const passport = require("passport");
+const session = require("express-session");
 
+//Swagger
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Zaragoza en juego",
+            version: "0.1.0",
+            description: "Jueguito divertido",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+            {
+                url: "http://localhost:3001",
+            },
+        ],
+    },
+    apis: ["./api/controllers/*.ts", "./api/models/*.ts"],
+};
+
+const specs = swaggerJsdoc(options);
 
 //Mongoose
 require("./api/models/db");
 
 var app = express();
 app.disable("x-powered-by");
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
@@ -32,13 +66,15 @@ app.use("/", indexRouter);
 app.use("/properties", propertiesRouter);
 app.use("/users", usersRouter);
 
-app.use(session({
-    secret: 'cat',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(
+    session({
+        secret: "cat",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
-app.use(passport.authenticate('session'));
-app.use('/api/auth', authRouter);
+app.use(passport.authenticate("session"));
+app.use("/api/auth", authRouter);
 
 export default app;
