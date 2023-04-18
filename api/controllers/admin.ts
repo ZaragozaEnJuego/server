21dfdb2181236acc80de7bf7f7946dc3a5b35134
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-18 11:34:33 +0200

    admin controller

---
+++ api/controllers/admin.ts
@@ -0,0 +1,40 @@
+import Users from "../models/users";
+import { Request, Response, response } from "express";
+
+const getUserList = (req: Request, res: Response) => {
+  Users.find()
+    .then((list) => res.status(200).json(list))
+    .catch((err) => res.status(500).json(err));
+};
+
+const updateAccess = (req: Request, res: Response) => {
+  if (req.params.id === undefined) {
+    res.status(400).json({
+      message: "user id is required",
+    });
+    return;
+  }
+
+  const { access } = req.body;
+
+  if (access === undefined) {
+    res.status(400).json({
+      message: "attribute access is required",
+    });
+    return;
+  }
+
+  Users.findByIdAndUpdate(
+    req.params.id,
+    { access },
+    { new: true } // devuelve el usuario actualizado
+  )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "User not found" })
+        : res.status(200).json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+};
+
+export { getUserList, updateAccess };
