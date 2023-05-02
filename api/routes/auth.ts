7e63896b4df3423d6d71daba8cf967a9fe064c93
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-02 20:04:30 +0200

    Auth (#29)
    * manada cookies
    
    * findOrCreateUser include
    
    * falla el cors
    
    * se manda jwt pero no esta el mail en ella
    
    * deberia ir asi
    
    * auth
    
    * no se
    
    * no se x
    
    * fix returns
    
    * Update passport.ts
    
    * commit
    
    ---------
    
    Co-authored-by: p3n4x0 <Penacho.AE@gmail.com>
    Co-authored-by: p3n4x0 <91628543+p3n4x0@users.noreply.github.com>

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -9,24 +9,36 @@ require("../../passport");
 const router = express.Router();
 
 router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+    "/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 router.get(
+    "/google/callback",
+    passport.authenticate("google", {
+        successRedirect: "https://client-hqzg5w9fc-785370-unizares.vercel.app",
+        failureRedirect: "https://client-hqzg5w9fc-785370-unizares.vercel.app",
+    })
+);
 
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req: any, res: any) => {
-    const token = jwt.sign(
-      { mail: req.body.mail },
-      'cat',
-      { expiresIn: '14h' }
-    );
+router.get(
+    "/google/login",
+    passport.authenticate(
+        "google",
+        {
+            failureRedirect: "http://localhost:5173/",
+        },
+        (req: any, res: any) => {
+            console.log(req.user);
 
-    //envio del JWT como respuesta al cliente
-    res.json({ token, isAdmin: false });
-  }
+            const token = jwt.sign({ mail: req.body.mail }, "cat", {
+                expiresIn: "14h",
+            });
+
+            //envio del JWT como respuesta al cliente
+            res.json({ token, isAdmin: false });
+        }
+    )
 );
 
 export default router;
