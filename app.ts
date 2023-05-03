ecbd4c535206840460ca6a85d6e3752ac4464c5d
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-03 19:01:15 +0200

    server URL

--- app.ts
+++ app.ts
@@ -17,6 +17,8 @@ const passport = require("passport");
 const session = require("express-session");
 const cors = require("cors");
 
+const serverUrl = process.env.SERVER_URL ?? "http://localhost:3000";
+
 //Swagger
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
@@ -35,13 +37,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
-            },
-            {
-                url: "http://localhost:3001",
-            },
-            {
-                url: "https://server-production-29b0.up.railway.app",
+                url: serverUrl,
             },
         ],
     },
@@ -55,56 +51,7 @@ require("./api/models/db");
 
 var app = express();
 app.disable("x-powered-by");
-/*
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
-    next();
-});
 
-app.use((req, res, next) => {
-    req.header("Access-Control-Allow-Credentials", true);
-    req.header("Access-Control-Allow-Origin", req.headers.origin);
-    req.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    req.header(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
-    next();
-});*/
-
-/*app.use("/api/auth/google/login", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Credentials: true",
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With,Content-Type, Accept"
-    );
-    next();
-});
-
-app.use("/api/auth/google/login", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header(
-        "Access-Control-Allow-Credentials: true",
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With,Content-Type, Accept"
-    );
-    next();
-});*/
-/*
-app.use(
-    cors({
-        origin: ["http://localhost:3000", "http://localhost:5173"],
-        credentials: true, // habilita el envío de credenciales
-    })
-);
-*/
 app.use(
     "/api-docs",
     swaggerUi.serve,
