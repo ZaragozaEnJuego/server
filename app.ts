83f7e6b061c29339c053060c75c30663d0799c86
p3n4x0 <Penacho.AE@gmail.com>
2023-05-04 19:51:42 +0200

    env

--- app.ts
+++ app.ts
@@ -69,7 +69,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
-    secret: process.env.SERVER_SECRET,
+    secret: 'cat',
     resave: false,
     saveUninitialized: false,
   })
