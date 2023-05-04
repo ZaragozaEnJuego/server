5f8777fe1bfc63c2a9a4f0fc26b0be24fd740487
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-04 13:53:11 +0200

    no cors

--- app.ts
+++ app.ts
@@ -52,16 +52,7 @@ require("./api/models/db");
 
 var app = express();
 app.disable("x-powered-by");
-app.use((req:Request, res:Response, next: NextFunction) => {
- // res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  next();
-});
+
 
 app.use(
   "/api-docs",
