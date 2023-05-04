8fe690de6ede34e36537298d716d67116b660daf
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-04 13:38:15 +0200

    no credentials header

--- app.ts
+++ app.ts
@@ -53,7 +53,7 @@ require("./api/models/db");
 var app = express();
 app.disable("x-powered-by");
 app.use((req:Request, res:Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Credentials", "true");
+ // res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
