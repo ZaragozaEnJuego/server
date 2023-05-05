b9564a1b6eb5edb3a65a57ded57d3f5d95b3d132
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-05 09:52:38 +0200

    await promise

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -12,70 +12,57 @@ const clientUrl = process.env.CLIENT_URL ?? "http://localhost:5173";
 
 // Rutas de autenticación con Google
 router.get(
-    "/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 router.get(
-    '/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req: Request, res: Response) => {
-      const token = jwt.sign(
-        { mail: req.body.mail },
-        'cat',
-        { expiresIn: '14h' }
-      );
-      const userId = getId(req.body.mail);
-      const isAdmin =  getIsAdmin(req.body.mail);
-      //envio del JWT como respuesta al cliente
-      //res.redirect(`${clientUrl}?token=${token}`)
-      res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=${isAdmin}`) ;
-    }
-  );
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  async (req: Request, res: Response) => {
+    const token = jwt.sign({ mail: req.body.mail }, "cat", {
+      expiresIn: "14h",
+    });
+    const userId = await getId(req.body.mail);
+    const isAdmin = await getIsAdmin(req.body.mail);
+    //envio del JWT como respuesta al cliente
+    //res.redirect(`${clientUrl}?token=${token}`)
+    res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=${isAdmin}`);
+  }
+);
 
 // Rutas de autenticación con GitHub
-router.get(
-  "/github", 
-  passport.authenticate("github")
-);
+router.get("/github", passport.authenticate("github"));
 
 router.get(
   "/github/callback",
-  passport.authenticate("github", { failureRedirect: '/api/auth/github' }),
+  passport.authenticate("github", { failureRedirect: "/api/auth/github" }),
   (req: Request, res: Response) => {
-    const token = jwt.sign(
-      { mail: req.body.mail },
-      'cat',
-      { expiresIn: '14h' }
-    );
+    const token = jwt.sign({ mail: req.body.mail }, "cat", {
+      expiresIn: "14h",
+    });
     const userId = getId(req.body.mail);
     //envio del JWT como respuesta al cliente
     //res.redirect(`${clientUrl}?token=${token}`)
-    res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=false`) ;
+    res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=false`);
   }
 );
 
 // Rutas de autenticación con Discord
-router.get(
-  "/discord", 
-  passport.authenticate("discord")
-);
+router.get("/discord", passport.authenticate("discord"));
 
 router.get(
   "/discord/callback",
-  passport.authenticate("discord", { failureRedirect: '/api/auth/discord' }),
+  passport.authenticate("discord", { failureRedirect: "/api/auth/discord" }),
   (req: Request, res: Response) => {
-    const token = jwt.sign(
-      { mail: req.body.mail },
-      'cat',
-      { expiresIn: '14h' }
-    );
+    const token = jwt.sign({ mail: req.body.mail }, "cat", {
+      expiresIn: "14h",
+    });
     const userId = getId(req.body.mail);
     //envio del JWT como respuesta al cliente
     //res.redirect(`${clientUrl}?token=${token}`)
-    res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=false`) ;
+    res.redirect(`${clientUrl}?token=${token}&id=${userId}&admin=false`);
   }
 );
-
 
 export default router;
