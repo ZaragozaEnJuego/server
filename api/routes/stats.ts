644d05e5434cc41d55a9bd19e649778141c1a2d8
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-05 11:05:33 +0200

    Revert "Weather merge" (#35)

--- api/routes/stats.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-import { getWeatherData } from "../controllers/stats";
-
-const router = Router();
-
-router
-    .get("/weather", getWeatherData)
-
-export default router;
