6c0b96214182ec18e94dcbb707af2c0551f0e74b
Ibón González Martínez de Apellániz <41486432+ibon-sk@users.noreply.github.com>
2023-05-08 13:01:05 +0200

    Update offers.ts

--- api/routes/offers.ts
+++ api/routes/offers.ts
@@ -8,8 +8,8 @@ import {
 const router = Router();
 
 router
-  .get("/negotiation/:id", getOffererOffers)
-  .get("/negotiation/:id", getOwnerOffers)
+  .get("/negotiation/:id/offerer", getOffererOffers)
+  .get("/negotiation/:id/owner", getOwnerOffers)
   .get("/negotiation/:id", getOffer);
 //.post("/negotiation", createOffer) queda pendiente por el momento
 
