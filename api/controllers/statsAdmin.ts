a9a5354886e4c5fb6d3ad7d89e397aa968b7d731
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-09 16:52:15 +0200

    Create statsAdmin.ts

---
+++ api/controllers/statsAdmin.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { PropertyPurchaseData } from "../models/statsAdmin";
+
+async function propertyBuys(req: Request, res: Response) {
+    //TODO: Implementar este servicio
+}
+async function getPropertiesByKind(req: Request, res: Response) {
+    //TODO: Implementar este servicio
+}
+
+async function collectPropertyPurchaseInfo(req: Request, res: Response) {
+    //TODO: Implementar este servicio
+}
+
+export { propertyBuys, getPropertiesByKind, collectPropertyPurchaseInfo }
