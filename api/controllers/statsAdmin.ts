255b021beab07405d4e1f7b0a2277c3a7323b09b
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2023-05-12 00:26:43 +0200

    Update statsAdmin.ts

--- api/controllers/statsAdmin.ts
+++ api/controllers/statsAdmin.ts
@@ -1,15 +1,43 @@
 import { Request, Response } from "express";
-import { PropertyPurchaseData } from "../models/statsAdmin";
+import PropertyPurchaseDataModel, { PropertyPurchaseData } from "../models/statsAdmin";
+import { Kind } from "../models/kindRules";
+import PropertieModel from "../models/properties";
 
 async function propertyPurchases(req: Request, res: Response) {
     //TODO: Implementar este servicio
 }
+
 async function getPropertiesByKind(req: Request, res: Response) {
-    //TODO: Implementar este servicio
+    interface IBody {
+        kind: Kind
+    }
+    const body: IBody = req.body
+    try {
+        const list = await PropertieModel.find({ kind: body.kind })
+        res.status(200).json(list)
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message })
+    }
 }
 
 async function collectPropertyPurchaseInfo(req: Request, res: Response) {
-    //TODO: Implementar este servicio
+    interface IBody {
+        property: string,
+        kind: Kind,
+        date: Date
+    }
+    const body: IBody = req.body
+    try {
+        const savedPurchaseData = await PropertyPurchaseDataModel.create({
+            property: body.property,
+            kind: body.kind,
+            date: body.date
+        })
+        res.status(201).json({ property: body.property })
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message })
+    }
+
 }
 
 export { propertyPurchases, getPropertiesByKind, collectPropertyPurchaseInfo }
