21c7ca6a7a2445a4b800fd1c83852f38661165b9
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-16 14:02:16 +0200

    ban

--- api/controllers/users.ts
+++ api/controllers/users.ts
@@ -89,93 +89,108 @@ import PropertieModel, { Propertie } from "../models/properties";
  *
  */
 const getUser = async (req: Request, res: Response) => {
-  if (req.params.id === undefined) {
-    res.status(400).json({ msg: "No id provided" });
-    return;
-  }
-  interface userDto {
-    id: string;
-    name: string;
-    icon?: string;
-    mail?: string;
-    liquidity: number;
-    //lastDayIncome: number;
-    properties: Propertie[];
-  }
-  const user = await UserModel.findById(req.params.id);
+    if (req.params.id === undefined) {
+        res.status(400).json({ msg: "No id provided" });
+        return;
+    }
+    interface userDto {
+        id: string;
+        name: string;
+        icon?: string;
+        mail?: string;
+        liquidity: number;
+        //lastDayIncome: number;
+        properties: Propertie[];
+    }
+    const user = await UserModel.findById(req.params.id);
 
-  if (user === null) {
-    res.status(404).json({ msg: "User does not exist" });
-    return;
-  }
-  const properties = await PropertieModel.find({ owner: req.params.id });
-  const userResponse: userDto = {
-    id: user._id,
-    name: user.name,
-    mail: user.mail,
-    liquidity: user.liquidity,
-    properties: properties,
-  };
-  res.status(200).json(userResponse);
+    if (user === null) {
+        res.status(404).json({ msg: "User does not exist" });
+        return;
+    }
+    const properties = await PropertieModel.find({ owner: req.params.id });
+    const userResponse: userDto = {
+        id: user._id,
+        name: user.name,
+        mail: user.mail,
+        liquidity: user.liquidity,
+        properties: properties,
+    };
+    res.status(200).json(userResponse);
 };
 
 const getIsAdmin = (mail: string) => {
-  return new Promise((resolve, reject) => {
-    UserModel.findOne({ mail: mail })
-      .then((user) => {
-        if (user) {
-          resolve(user.admin);
-          //console.log(`El usuario encontrado es: ${user}`);
-          logger.info("usuario encontrado: " + mail + "/" + user);
-        } else {
-          reject(mail);
-        }
-      })
-      .catch((err) => reject(err));
-  });
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ mail: mail })
+            .then((user) => {
+                if (user) {
+                    resolve(user.admin);
+                    //console.log(`El usuario encontrado es: ${user}`);
+                    logger.info("usuario encontrado: " + mail + "/" + user);
+                } else {
+                    reject(mail);
+                }
+            })
+            .catch((err) => reject(err));
+    });
 };
 
 const getId = (mail: string) => {
-  return new Promise((resolve, reject) => {
-    UserModel.findOne({ mail: mail })
-      .then((user) => {
-        if (user) {
-          resolve(user._id);
-          //console.log(`El usuario encontrado es: ${user}`);
-        } else {
-          reject(mail);
-        }
-      })
-      .catch((err) => reject(err));
-  });
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ mail: mail })
+            .then((user) => {
+                if (user) {
+                    resolve(user._id);
+                    //console.log(`El usuario encontrado es: ${user}`);
+                } else {
+                    reject(mail);
+                }
+            })
+            .catch((err) => reject(err));
+    });
+};
+
+const getAcces = (mail: string) => {
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ mail: mail })
+            .then((user) => {
+                if (user) {
+                    resolve(user.access);
+                    //console.log(`El usuario encontrado es: ${user}`);
+                } else {
+                    reject(mail);
+                }
+            })
+            .catch((err) => reject(err));
+    });
 };
 
 const findOrCreateUser = (name: string, mail: string, admin: boolean) => {
-  return new Promise((resolve, reject) => {
-    UserModel.findOne({ mail: mail })
-      .then((user) => {
-        if (user) {
-          resolve(user);
-          //console.log(`El usuario encontrado es: ${user}`);
-        } else {
-          //console.log(`No se encontró ningún usuario con el correo ${mail}`);
-          UserModel.create({
-            name: name,
-            liquidity: 4000000,
-            mail: mail,
-            admin: admin,
-          })
-            .then((newUser) => {
-              //console.log(`Creado correctamente. ${newUser}`);
-              resolve(newUser);
+    return new Promise((resolve, reject) => {
+        UserModel.findOne({ mail: mail })
+            .then((user) => {
+                if (user) {
+                    resolve(user);
+                    //console.log(`El usuario encontrado es: ${user}`);
+                } else {
+                    //console.log(`No se encontró ningún usuario con el correo ${mail}`);
+                    UserModel.create({
+                        name: name,
+                        liquidity: 4000000,
+                        mail: mail,
+                        admin: admin,
+                    })
+                        .then((newUser) => {
+                            //console.log(`Creado correctamente. ${newUser}`);
+                            resolve(newUser);
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        });
+                }
             })
-            .catch((err) => {
-              reject(err);
-            });
-        }
-      })
-      .catch((err) => reject(err));
-  });
+            .catch((err) => reject(err));
+    });
 };
 
-export { getUser, findOrCreateUser, getId, getIsAdmin };
+export { getUser, findOrCreateUser, getId, getIsAdmin, getAcces };
